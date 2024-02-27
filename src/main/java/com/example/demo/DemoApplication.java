640ef28a72941c5b89bad1905d056602420a8f0e
zzzcolcol <zzzcolcol19@naver.com>
2024-02-27 15:41:51 +0900

    change index.html

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,25 +3,23 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.StringReader;
 
-@RestController
+
+
+
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
-	@GetMapping(value = "/")
-	public String doGetHelloWorld(){
-		return "Hello World";
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
 	}
 
-	@GetMapping(value = "/demo")
-	public String doGetHelloWorldDemo(){
-		return "Hello World (demo)";
-	}
 }
